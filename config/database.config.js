57a88f3110ed57f923b14a186a4d20776d389ef5
NJinwala <83692093+NJinwala@users.noreply.github.com>
2021-05-05 13:31:49 +0530

    Added API calls for user registration and login

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     /*url: 'mongodb+srv://reactapp:reactjstomongodb123@afc-cluster1.tjxtf.mongodb.net/react_app_db?retryWrites=true&w=majority' */
-url:'mongodb+srv://pcuser:pcuser_mongodb@cluster0.a9z0q.mongodb.net/pc_data?retryWrites=true&w=majority'
+url:'mongodb+srv://pcuser:pcuser_mongodb@cluster0.a9z0q.mongodb.net/pc_data?retryWrites=true&w=majority',
+secretOrKey : "secret"
 }
